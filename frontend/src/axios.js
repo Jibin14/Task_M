75be90a6fa4262406828a133be46968d9a594c8b
Jibin Shiju <jibinshiju12@gmail.com>
2026-05-19 14:57:20 +0530

    updated api url

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://task-m-zvtd.onrender.com/api/v1",
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
