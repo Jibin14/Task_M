392409900da1ea425dc462c24bd1c18289202ae2
Jibin Shiju <jibinshiju12@gmail.com>
2026-05-19 14:21:38 +0530

    updated api url

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: [
-  "http://localhost:5173",
-  "https://task-management-application-pr46xsdus.vercel.app",
-],
+    origin: "https://task-2ph7t7xh6-jibinshiju12-1796s-projects.vercel.app",
     credentials: true,
   })
 );
